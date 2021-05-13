95654d6bae92f44f6cc66ab496947b692b962ad4
nazy67 <nazykh67@gmail.com>
2021-05-12 22:37:08 -0500

    modified lt_and_security_groups.tf

--- modules/webserver/lt_and_security_grps.tf
+++ modules/webserver/lt_and_security_grps.tf
@@ -1,4 +1,4 @@
-### Lauch Template
+# Launch Template
 resource "aws_launch_template" "web_template" {
   name_prefix   = "${local.prefix}-template-" # when you close it with '-' the rest of the name will be created by aws and will be unique
   image_id      = data.aws_ami.amazon_linux2.image_id
@@ -12,7 +12,7 @@ resource "aws_launch_template" "web_template" {
   vpc_security_group_ids = [aws_security_group.webserver_sg.id]
 }
 
-### Webserver Security Group
+# Webserver Security Group
 resource "aws_security_group" "webserver_sg" {
   name        = "${local.prefix}-sg"
   description = "Allow http inbound traffic to alb"
@@ -36,7 +36,7 @@ resource "aws_security_group_rule" "webserver_egress" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.webserver_sg.id
 }
-### ALB Sec Group
+# ALB Sec Group
 resource "aws_security_group" "web_alb_sg" {
   name        = "${local.prefix}-app_lb_sg"
   description = "Allow http and https inbound traffic"
