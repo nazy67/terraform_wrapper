7716f3e3e7864c684f3c532b7699e74301cd26bd
nazy67 <nazykh67@gmail.com>
2021-05-13 22:33:48 -0500

    modified backend.tf, main.tf

--- rds/backend.tf
+++ rds/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-  bucket = "terraform-nazy-state"
-  key = "__env__/rds.tfstate"
-  region = "us-east-1"
-  dynamodb_table = "terraform-state-locks"
-  } 
+    bucket         = "terraform-nazy-state"
+    key            = "__env__/rds.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+  }
 }
