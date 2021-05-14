d3f2e45ef3145cfb18da1c011474e5a72e208ad7
nazy67 <nazykh67@gmail.com>
2021-05-13 22:34:09 -0500

    modified variables.tf

--- rds/variables.tf
+++ rds/variables.tf
@@ -1,25 +1,25 @@
 variable "env" {
-    description = "the name of the environment"
-    type = string
-    default = "dev"
-} 
+  description = "the name of the environment"
+  type        = string
+  default     = "dev"
+}
 
 variable "storage" {
-    description = "the storage size of database"
-    type = string
+  description = "the storage size of database"
+  type        = string
 }
 
 variable "skip_snapshot" {
-    description = "it will skip the final snapshot incase if someone deletes database by accident"
-    type = bool
+  description = "it will skip the final snapshot incase if someone deletes database by accident"
+  type        = bool
 }
 
 variable "instance_class" {
-    description = " database instance class"
-    type = string
+  description = " database instance class"
+  type        = string
 }
 
 variable "username" {
-    description = "the username of database"
-    type = string
+  description = "the username of database"
+  type        = string
 }
