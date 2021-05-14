e487998ec6c9f45a05b8571a42d5553fe67b8706
nazy67 <nazykh67@gmail.com>
2021-05-13 23:26:33 -0500

    modified  README.md

--- webserver/tfvars/dev.tf
+++
@@ -1,8 +0,0 @@
-  env = "dev" 
-  instance_type = "t2.micro"
-  
-
-  bucket = "terraform-nazy-state"
-  key = "dev/webserver.tfstate"
-  region = "us-east-1"
-  dynamodb_table = "terraform-state-locks" 
